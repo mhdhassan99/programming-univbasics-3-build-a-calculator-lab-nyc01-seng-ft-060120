d62916c85f3a3ebd45baa96c4fb227b853942c6a
Muhammad Hassan <mhassan.work99@gmail.com>
2020-05-18 19:39:10 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  result = num2 + num1
+  return result
 end
 
 def subtraction(num1, num2)
-
+  result = (num2 - num1)
+  return result.abs
 end
 
 def division(num1, num2)
-
+  result = (num1 / num2)
+  return result.abs
 end
 
 def multiplication(num1, num2)
-
+  result = num1 * num2
+  return result
 end
 
 def modulo(num1, num2)
-
+  result = num1 % num2
+  return result.abs
 end
 
 def square_root(num)
-
+  return Math.sqrt(num)
 end
