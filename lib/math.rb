5fc0f7e641821f604b115f1f34b86ce68da5f131
Muhammad Hassan <mhassan.work99@gmail.com>
2020-05-18 19:28:40 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,25 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  result = num2 - num1
+  return result
 end
 
 def division(num1, num2)
-
+  result = num2 / num1
+  return result
 end
 
 def multiplication(num1, num2)
-
+  result = num1 * num2
+  return result
 end
 
 def modulo(num1, num2)
-
+  result = num2 % num1 
+  return result
 end
 
 def square_root(num)
-
+  return Math.sqrt(num)
 end
